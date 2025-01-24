a0446dda7527f1797b804fc79a30839a01c8ae00
yummygyudon <bang2brew@gmail.com>
2025-01-25 02:12:34 +0900

    feat : code - condition statement

---
+++ src/statement/condition_if.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+num_a = 2
+num_b = 12
+num_c = 10
+num_d = 3
+
+if num_a % 2 == 0
+  if num_a > 10
+    puts "even bigger than 10"
+  elsif num_a < 10
+    puts "even smaller than 10"
+  else
+    puts "even 10"
+  end
+else
+  puts "odd"
+end
+
+if num_b % 2 == 0
+  if num_b > 10
+    puts "even bigger than 10"
+  elsif num_b < 10
+    puts "even smaller than 10"
+  else
+    puts "even 10"
+  end
+else
+  puts "odd"
+end
+
+if num_c % 2 == 0
+  if num_c > 10
+    puts "even bigger than 10"
+  elsif num_c < 10
+    puts "even smaller than 10"
+  else
+    puts "even 10"
+  end
+else
+  puts "odd"
+end
+
+if num_d % 2 == 0
+  if num_d > 10
+    puts "even bigger than 10"
+  elsif num_d < 10
+    puts "even smaller than 10"
+  else
+    puts "even 10"
+  end
+else
+  puts "odd"
+end
+# unless  # => 조건이 거짓일때
+#   else # => unless 조건이 참일때
+# end
---
+++ src/statement/condition_unless.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+num_a = 2
+num_b = 12
+num_c = 10
+num_d = 3
+
+unless num_a % 2 == 0 # => 조건이 거짓일때
+  puts "odd"
+else # => unless 조건이 참일때
+  puts "even"
+end
+
+unless num_b % 2 == 0
+  puts "odd"
+else
+  puts "even"
+end
+
+unless num_c % 2 == 0
+  puts "odd"
+else
+  puts "even"
+end
+
+unless num_d % 2 == 0
+  puts "odd"
+else
+  puts "even"
+end
